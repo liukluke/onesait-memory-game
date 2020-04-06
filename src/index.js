62a310226ff8bdbdac002516cc7d4b28a0f25fec
lmanfredi <lmanfredi@minsait.com>
2020-04-06 12:52:08 +0200

    add sass

--- src/index.js
+++ src/index.js
@@ -1,2 +1,1 @@
-const arr = new Array(4).fill('').map((e, i) => e = i)
-console.log(arr)
+import './assets/sass/main.sass';
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,10 @@ module.exports = {
                 test: /\.(js)$/,
                 exclude: /node_modules/,
                 use: ['babel-loader']
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     },
